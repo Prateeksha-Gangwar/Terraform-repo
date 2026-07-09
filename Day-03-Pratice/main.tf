81600bdabef0087d879ebf8fd53edff20e9d44fd
Prateeksha-Gangwar <prateekshaganwar1@gmail.com>
2026-07-09 14:22:49 +0530

    Terraform files

---
+++ Day-03-Pratice/main.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "name" {
+    ami           = var.ami
+    instance_type = var.insv
+    tags = {
+        Name = var.tags
+    }
+  
+}
---
+++ Day-03-Pratice/terraform.tfvars
@@ -0,0 +1,3 @@
+ami = "ami-002192a70217ac181"
+insv = "t2.medium"
+tags = "DB-Server1"
