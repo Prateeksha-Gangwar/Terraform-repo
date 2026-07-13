6df427a72dbb101df3ec1d13922555006f7274e9
Prateeksha-Gangwar <prateekshaganwar1@gmail.com>
2026-07-13 15:14:46 +0530

    med

--- Day-07-state-locking/main.tf
+++ Day-07-state-locking/main.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "name" {
   ami = "ami-01edba92f9036f76e"
-  instance_type = "t2.micro"
+  instance_type = "t2.medium"
   tags = {
-    Name = "Terraform-Instance-ready"
+    Name = "Terraform-Instance-ready-template"
   }
 }
 
