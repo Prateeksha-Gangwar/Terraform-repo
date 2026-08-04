2678de3be5b02f3f829b4698d6af56a84cdd5cea
Prateeksha-Gangwar <prateekshagangwar1@gmail.com>
2026-08-04 15:48:09 +0530

    Update main.tf

--- Jenkins/main.tf
+++ Jenkins/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "name" {
     ami = "ami-02b64aa047cb5edf5"
     instance_type = "t3.micro"
     tags = {
-      Name = "MDKKKKSSSSSS-Server"
+      Name = "Cloud-Server"
     }
   
 }
