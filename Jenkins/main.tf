cf15e4cb90ca223c53f3eb129914ff686f880adc
Prateeksha-Gangwar <prateekshagangwar1@gmail.com>
2026-09-11 16:32:22 +0530

    Update main.tf

--- Jenkins/main.tf
+++ Jenkins/main.tf
@@ -1,8 +1,12 @@
-resource "aws_instance" "name" {
-  ami           = "ami-0354c98ae10b02961"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "CloudINIDevopsecops"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
   }
+}
+
+provider "aws" {
+  region = "us-east-1"
 }
