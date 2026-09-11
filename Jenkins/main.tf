df5840178ebfcdedc126a0927f99fa75a5069112
Prateeksha-Gangwar <prateekshagangwar1@gmail.com>
2026-09-11 17:09:57 +0530

    Update main.tf

--- Jenkins/main.tf
+++ Jenkins/main.tf
@@ -1,12 +1,8 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.0"
-    }
-  }
-}
+resource "aws_instance" "devops_ec2" {
+  ami           = "ami-0354c98ae10b02961"
+  instance_type = "t2.micro"
 
-provider "aws" {
-  region = "us-east-1"
+  tags = {
+    Name = "Jenkins-CICD-EC2"
+  }
 }
