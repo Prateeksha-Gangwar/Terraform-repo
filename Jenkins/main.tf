fd34b865d01b25545b81078d78557914fb85345a
Prateeksha-Gangwar <prateekshagangwar1@gmail.com>
2026-09-11 16:26:16 +0530

    Format aws_instance resource in main.tf

--- Jenkins/main.tf
+++ Jenkins/main.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "name" {
-    ami = "ami-0354c98ae10b02961"
-    instance_type = "t2.micro"
-    tags = {
-      Name = "CloudINIDevopsecops"
-    }
-  
+  ami           = "ami-0354c98ae10b02961"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "CloudINIDevopsecops"
+  }
 }
